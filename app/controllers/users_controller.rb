15829ef9168daf0ae7fded3ce3d6deb532fb4bbf
Dmitry Khromov <devandart@ya.ru>
2015-10-23 01:41:07 +0300

    edit for users, check point

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
   def index
     @users = User.all.order(:created_at)
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(user_params)
+    if @user.save
+      redirect_to users_path, notice: 'User was updated'
+    else
+      render action: :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, emails: [], phone_numbers: [])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ActiveRecord::Base
   serialize :emails, Array
   serialize :phone_numbers, Array
+
+  before_save :uniq_emails
+  before_save :uniq_phone_numbers
+
+  private
+
+  def uniq_emails
+    self.emails = (self.emails + emails).uniq
+  end
+
+  def uniq_phone_numbers
+    self.emails = (self.emails + emails).uniq
+  end
 end
