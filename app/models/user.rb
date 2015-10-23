60299e847e0b29d5caefbaccea3be570eda57465
Dmitry Khromov <devandart@ya.ru>
2015-10-23 14:47:48 +0300

    some fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,8 +32,8 @@ class User < ActiveRecord::Base
   def self.to_csv(options = {})
     CSV.generate(options) do |csv|
       csv << column_names
-      all.each do |product|
-        csv << product.attributes.values_at(*column_names)
+      all.each do |user|
+        csv << user.attributes.values_at(*column_names)
       end
     end
   end
